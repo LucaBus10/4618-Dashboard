1ee8a85d86a77156ddbcdaa8b52c4ae5df1712af
Gulksie <shaker02@live.ca>
2018-01-12 21:55:50 -0500

    Added simple hello world

--- 4618-Dashboard/src/main/java/Main.java
+++ 4618-Dashboard/src/main/java/Main.java
@@ -1,7 +1,39 @@
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-    public static void main() {
-        System.out.println("Dummy");
-        //TODO everything
+/* This makes a window with one button that prints "Hello world" to the console
+ */
+
+public class Main extends Application{
+    //don't know yet if ill need to keep this here or just in the function but we'll see
+    StackPane root;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) {
+        root = new StackPane();
+
+        window.setTitle("Dashboard");
+
+        //create new button and assign some code to it
+        Button btn = new Button("Hello world"); //this is actually really similar to TKinter
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello world!");
+            }
+        });
+
+        root.getChildren().add(btn); //i guess add the button to the window
+        window.setScene(new Scene(root, 1280, 720)); //create new window, specifying resolution
+        window.show();
     }
 }
