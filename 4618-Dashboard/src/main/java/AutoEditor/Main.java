da267d7bee798b0a56f657613c98de05316060a0
Gulksie <shaker02@live.ca>
2018-01-16 19:19:08 -0500

    Made simple top down view of field in new AutoEditor package

---
+++ 4618-Dashboard/src/main/java/AutoEditor/Main.java
@@ -0,0 +1,42 @@
+package AutoEditor;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        // load the image
+        Image image = new Image("https://image.ibb.co/c0a5NR/field.png"); //couldn't get it to load
+        //an image off of disk for some reason...
+
+        // simple displays ImageView the image as is
+        ImageView iv1 = new ImageView();
+        iv1.setImage(image);
+
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        scene.setFill(Color.BLACK);
+        HBox box = new HBox();
+        box.getChildren().add(iv1);
+        root.getChildren().add(box);
+
+        stage.setTitle("ImageView");
+        stage.setWidth(415);
+        stage.setHeight(200);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
+
